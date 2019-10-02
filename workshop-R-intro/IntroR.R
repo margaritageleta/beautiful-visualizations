3649a7dd92627097d3063464d3af91e9e80d8c38
3omni <noticiasmundiales99@gmail.com>
2019-10-02 15:47:07 +0200

    intro

---
+++ workshop-R-intro/IntroR.R
@@ -0,0 +1,32 @@
+# PART 1 - R WORKSHOP #
+# =================== #
+# Well, the first comment is that with "#" you can add comments!
+
+# Math works in R as anywhere else
+4 + 2
+4 - 2
+4 * 2
+4 / 2
+4 ^ 2
+4 %/% 2      # What is that ... ?
+4 %% 2
+
+# Variable assignment
+x <- 4
+y <- 5 %/% 2 # What is the value of y ... ?
+z <- x %% y  # Guess the value of z 
+# To print the assignment use ()
+(t <- x)
+
+# Some cool things 
+4 -> x       # does also work! not the usual way, though
+i <- j <- 3  # what is the value of i and j ?
+i <- 3 + (j <- 4)
+k <- i + (j <- x) # what is the value of k ?
+
+
+
+
+
+
+
